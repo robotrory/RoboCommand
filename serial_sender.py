102e7eac2a047682fbe859a70177ab091380418d
benallen <external@benallen.info>
2016-05-21 19:29:28 +0100

    Added changes

--- serial_sender.py
+++ serial_sender.py
@@ -1,17 +1,12 @@
 import serial
 import time
-# port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=3.0)
+port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=3.0)
 
 def sendServo(servoNum, degree):
   print("")
-  # port.write('(%s,%s)' % (servoNum, degree))
+  port.write('(%s,%s)' % (servoNum, degree))
 
 def sendMessage(message):
   print("")
   # message = message.replace('|', '')
-  # port.write('(6,%s)' % (message))
-
-
-
-
-
+  port.write('(6,%s)' % (message))
